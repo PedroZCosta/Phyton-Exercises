4641dcf9e21aa7358a1a7327cf0efd0437794cbc
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2024-04-01 22:15:14 -0300

    Estruturas_condicionais_Exec6.py

--- Estruturas_condicionais_Exec6.py
+++ Estruturas_condicionais_Exec6.py
@@ -1,25 +1,44 @@
+"""
+Exercicio: Código de Exibição de Data
+Criador: Pedro Henrique Zakrzevski Costa
+Data: 30/03/2024
+versao: 1.0
+"""
 # Informacoes do usuario
 user_day = int(input("Por favor, digite seu dia de nascimento: "))
-user_month = int(input("Por favor, digite seu mes de nascimento (Em extenso): "))
+user_month = input("Por favor, digite seu mes de nascimento (Em extenso): ")
 user_year = int(input("Por favor, digite seu dia de nascimento (Com 4 digitos): "))
 date_display = input("Voce deseja que a data seja exibida de maneira simples, abreviada ou completa? ")
-# conversao de meses em nome para numero
-janeiro = "01"
-fevereiro = "02"
-marco = "03"
-abril = "04"
-maio = "05"
-junho = "06"
-julho = "06"
-agosto = "08"
-setembro = "09"
-outubro = "10"
-novembro = "11"
-dezembro = "12"
 # resolucao da forma de exibicao da data
 if date_display == "simples":
+    while date_display:
+        janeiro = "01"
+        fevereiro = "02"
+        marco = "03"
+        abril = "04"
+        maio = "05"
+        junho = "06"
+        julho = "06"
+        agosto = "08"
+        setembro = "09"
+        outubro = "10"
+        novembro = "11"
+        dezembro = "12"
     print(f"{user_day}/{user_month}/{user_year}")
 elif date_display == "abreviada":
+    while date_display == "abreviada":
+        janeiro = "jan"
+        fevereiro = "fav"
+        marco = "mar"
+        abril = "abr"
+        maio = "mai"
+        junho = "jun"
+        julho = "jul"
+        agosto = "ago"
+        setembro = "set"
+        outubro = "out"
+        novembro = "nov"
+        dezembro = "dez"
     print(f"{user_day}/{user_month}/{user_year}")
 else:
     print(f"{user_day}/{user_month}/{user_year}")

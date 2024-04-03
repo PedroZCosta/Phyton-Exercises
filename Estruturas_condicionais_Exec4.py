2295ba6c52d6d32c52977cf50ef8b7059f03b931
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2024-04-02 21:29:23 -0300

    Estruturas_condicionais_Exec4.py

--- Estruturas_condicionais_Exec4.py
+++ Estruturas_condicionais_Exec4.py
@@ -1,3 +1,9 @@
+"""
+Exercicio: destino do passageiro
+Criador: Pedro Henrique Zakrzevski Costa
+Data: 22/03/2024
+versao: 1.0
+"""
 local_travel = input("Que regiao voce deseja viajar? (norte, nordeste, centro-oeste ou sul) ")
 round_trip = input("Voce deseja fazer um  viagem de ida e volta? (s/n) ")
 # precos fixos das viagens de ida
@@ -6,15 +12,20 @@ north = 500
 northeast = 350
 midwest = 350
 south = 300
+# precos adicionais da volta
+north_back = 400
+northeast_back = 300
+midwest_back = 250
+south_back = 250
 # calculo do preco com a ida e a volta
 if round_trip == "s" and local_travel == "norte":
-    travel_price = north + 400
+    travel_price = north + north_back
 elif round_trip == "s" and local_travel == "nordeste":
-    travel_price = northeast + 300
+    travel_price = northeast + northeast_back
 elif round_trip == "s" and local_travel == "centro_oeste":
-    travel_price = midwest + 250
+    travel_price = midwest + midwest_back
 elif round_trip == "s" and local_travel == "sul":
-    travel_price = south + 250
+    travel_price = south + south_back
 else:
     if local_travel == "norte":
         travel_price = north

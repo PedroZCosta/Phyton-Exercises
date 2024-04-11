6a7ece4420234f783efaf696e91ef9a71df97560
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2024-04-11 20:50:49 -0300

    Exercicios_lacos4.py

--- Exercicios_lacos4.py
+++ Exercicios_lacos4.py
@@ -4,10 +4,28 @@ Criador: Pedro Henrique Zakrzevski Costa
 Data: 09/04/2024
 Versao: 1.0
 """
-
-numbers = []
-for i in range(5):
-    num = int(input("Por favor, digite 5 numeros: "))
-    numbers.append(num)
-    numbers.sort()
-print(numbers)
+num1 = float(input("digite o primeiro numero: "))
+num2 = float(input("digite o segundo numero: "))
+num3 = float(input("digite o terceiro numero: "))
+num4 = float(input("digite o quarto numero: "))
+num5 = float(input("digite o quinto numero: "))
+while True:
+    if num1 < num2 <num3 <num4 < num5:
+        break
+    elif num1 > num2:
+        x = num1
+        num1 = num2
+        num2 = x
+    elif num2 > num3:
+        x = num2
+        num2 = num3
+        num3 =x
+    elif num3 > num4:
+        x = num3
+        num3 = num4
+        num4 = x
+    elif num4 > num5:
+        x = num4
+        num4 = num5
+        num5 = x
+print(f"{num1},{num2},{num3},{num4},{num5}")
